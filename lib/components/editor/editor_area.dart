35852d3cbda7f14a3322a8a5966e97d5aacbc289
tronxi <sergio.gsanchez97@gmail.com>
2024-02-10 13:35:32 +0100

    fix selected area

--- lib/components/editor/editor_area.dart
+++ lib/components/editor/editor_area.dart
@@ -77,30 +77,26 @@ class _EditorAreaPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     String text =
         editorController.getOpened(fileModel)!.currentFileController.value.text;
-    final textStyle = TextStyle(
-      color: lineNumberColor,
-      fontSize: 12,
-    );
-    final textPainter = TextPainter(
-      text: TextSpan(style: textStyle),
-      textDirection: TextDirection.ltr,
-    )..layout();
 
     int lineNumber = _calculateLineNumber(currentOffset, text);
-    double y = _calculateLineNumberPosition(textPainter, lineNumber) + 6;
+    double y = _calculateSelectedLineNumberPosition(lineNumber);
     final Paint paint = Paint()
       ..color = selectedLineColor
-      ..strokeWidth = 16;
+      ..strokeWidth = 18;
     final Offset start = Offset(0, y);
     final Offset end = Offset(size.width, y);
     canvas.drawLine(start, end, paint);
 
     for (int i = 1; i <= _getTotalLines(text); i++) {
-      textPainter.text = TextSpan(
-        text: i.toString(),
-        style: textStyle,
-      );
-      textPainter.layout();
+      final textPainter = TextPainter(
+        text: TextSpan(
+            text: i.toString(),
+            style: TextStyle(
+              color: lineNumberColor,
+              fontSize: 12,
+            )),
+        textDirection: TextDirection.ltr,
+      )..layout();
       final offset = Offset(8, _calculateLineNumberPosition(textPainter, i));
       textPainter.paint(canvas, offset);
     }
@@ -126,6 +122,13 @@ class _EditorAreaPainter extends CustomPainter {
     const offset = 4;
     final textHeight = textPainter.height;
     double y = (lineHeight * i) + (lineHeight / 2) - (textHeight / 2);
+    return y - offset;
+  }
+
+  double _calculateSelectedLineNumberPosition(int i) {
+    const lineHeight = 14;
+    const offset = 4;
+    double y = (lineHeight * i) + (lineHeight / 2);
     return y - offset;
   }
 }
