a72e638ec3f79f62461c4dda7e68b0dcc8eac50a
tronxi <sergio.gsanchez97@gmail.com>
2024-02-29 00:34:42 +0100

    add colors

--- lib/shared/colors.dart
+++ lib/shared/colors.dart
@@ -1,3 +1,4 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 
 extension Colors on ThemeData {
@@ -47,5 +48,24 @@ extension Colors on ThemeData {
 
   Color get editorFileTab {
     return const Color.fromRGBO(37, 53, 56, 1);
+  }
+
+  WindowButtonColors get windowButtonColor {
+    return WindowButtonColors(
+        iconNormal: const Color(0xFF805306),
+        iconMouseOver: const Color(0xFF805306),
+        iconMouseDown: const Color(0xFFFFD500),
+        mouseOver: const Color(0xFFF6A00C),
+        mouseDown: const Color(0xFF805306));
+  }
+
+  WindowButtonColors get windowCloseButtonColor {
+    return WindowButtonColors(
+      iconNormal: const Color(0xFF805306),
+      iconMouseOver: const Color(0xFF805306),
+      iconMouseDown: const Color(0xFFFFD500),
+      mouseOver: const Color(0xFFF6A00C),
+      mouseDown: const Color(0xFF805306),
+    );
   }
 }
